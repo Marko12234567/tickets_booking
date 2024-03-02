540deb98ae24bd89fa1d004d4e82133263a57d20
Marko12234567 <markogatalo2008@gmail.com>
2024-03-02 14:46:37 +0200

    Перший комміт

---
+++ booking/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+    
+    class Meta:
+        ordering = ['name']
+
+
+class Place(models.Model):
+    sector = models.IntegerField()
+    row = models.IntegerField()
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f'Сектор: {self.sector}, Ряд: {self.row}, Місце: {self.number}'
+    
+
+    class Meta:
+        unique_together = (('sector', 'row', 'number'))
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'Імя: {self.user}, Місце: {self.place}, Дата: {self.date}'
